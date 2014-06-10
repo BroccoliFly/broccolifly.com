86330ca5b1db843682aaa66ff46dbc673859488b
Alexander Selzer <aselzer3@gmail.com>
2014-06-10 16:18:55 +0200

    Fly animation improvement + design fixes & hiding navbar

--- js/app.js
+++ js/app.js
@@ -1,21 +1,36 @@
-var pos = 0;
+$.fn.circle = function() {
+  var element = this;
+  var pos = 0;
+  (function animate() {
+      pos += 0.04;
 
-function rotate() {
-    pos += 0.04;
+      var radius = 20;
+      var marginLeft = ($(window).width() / 2) - 160;
+      var marginTop = 0;
 
-    var radius = 20;
-    var marginLeft = ($(window).width() / 2) - 160;
-    var marginTop = 0;
+      var dx = (radius * Math.cos(pos));
+      var dy = (radius * Math.sin(pos));
 
-    var left = Math.floor(marginLeft + (radius * Math.cos(pos)));
-    var top = Math.floor(marginTop + (radius * Math.sin(pos)));
+      var fly = element[0];
 
-    $('.fly').animate({
-        "margin-top": top,
-        "margin-left": left,
-    }, 1, function() {
-        rotate();
-    });
+      var translate = "translate(" + dx + "px," + dy + "px" + ")";
+      var scale = "scale(" + (0.8 + Math.abs(Math.sin(pos)) * 0.2) + ")";
+
+      fly.style.transform = scale + " " + translate;
+      fly.style.webkitTransform = scale + " " + translate;
+      fly.style.mozTransform = scale + " " + translate;
+      fly.style.msTransform = scale + " " + translate;
+
+      (requestAnimationFrame
+        || webkitRequestAnimationFrame
+        || mozRequestAnimationFrame
+        || setTimeout
+      )(animate, 40);
+  })();
 }
 
-rotate();
+if ($(".fly").length > 0) {
+  $(".fly").circle();
+}
+
+$(".navbar").headroom({"offset": 100});
